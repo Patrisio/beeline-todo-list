394225661860b212bb27d38f25bdc72673217921
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-07-19 11:21:43 +0300

    firebase deplot configurations

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
